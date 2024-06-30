3b29f2d1b365577f0ac820b1d299b386985d2dcd
Jeremiah Gutherie <37122175+PennStateLefty@users.noreply.github.com>
2024-06-30 12:09:07 -0400

    Added try-catch and more hints to app.run

--- app.py
+++ app.py
@@ -9,20 +9,26 @@ logging.basicConfig(level=logging.INFO)
 
 @app.route('/')
 def show_auth_headers():
-    logging.info('Received request')
-    # List of Azure EasyAuth headers you are interested in
-    auth_headers = [
-        'X-MS-TOKEN-AAD-ID-TOKEN',
-        'X-MS-CLIENT-PRINCIPAL-NAME',
-        'X-MS-CLIENT-PRINCIPAL-ID'
-    ]
-    
-    # Capture and store these headers if they exist in the request
-    captured_headers = {header: request.headers.get(header) for header in auth_headers if request.headers.get(header)}
-    
-    # Return the captured headers as a JSON response
-    return jsonify(captured_headers)
+    try:
+        logging.info('Received request')
+        # List of Azure EasyAuth headers you are interested in
+        auth_headers = [
+            'X-MS-TOKEN-AAD-ID-TOKEN',
+            'X-MS-CLIENT-PRINCIPAL-NAME',
+            'X-MS-CLIENT-PRINCIPAL-ID'
+        ]
+        
+        print(f"Received a request - logging to print command")
+
+        # Capture and store these headers if they exist in the request
+        captured_headers = {header: request.headers.get(header) for header in auth_headers if request.headers.get(header)}
+        
+        # Return the captured headers as a JSON response
+        return jsonify(captured_headers)
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+        return jsonify({'error': 'An error occurred'})
 
 if __name__ == '__main__':
     # Run the app on the port defined by the 'PORT' environment variable, or 5000 if not defined
-    app.run()
+    app.run(host="0.0.0.0", port=8000)
